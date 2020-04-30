ffe5f54c7699e03ef19e0438bfbbb7b0e6a00278
Kharak Singh Kular <kharakkular2811@gmail.com>
2020-04-30 16:17:06 -0400

    Updated original file

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,6 @@
 public class HelloWsdsadasdppsdnsakdlaorld{
 	public static void main(String[] args){
 		System.out.println("Hello World");	
-	//	Prints "Welcome to the Git";
 	}	
-//adding comments and adding second comment
-
 
 }
